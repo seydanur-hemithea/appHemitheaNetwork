ef821a89ef54ad1a4c3610dfe209ae623e3c1fd0
Hemithea <seydanuraydin50@gmail.com>
2026-04-15 08:47:51 +0300

    Fix syntax error in engine creation

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if not hasattr(bcrypt, "__about__"):
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL)
-)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
